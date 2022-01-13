b34961fd6f73106b4890113e964287b2ebb6dfec
LMS <leejunseok95@gmail.com>
2022-01-13 21:11:58 +0900

    Jwt Security update

--- src/main/java/com/example/dditlms/security/JwtSecurityService.java
+++ src/main/java/com/example/dditlms/security/JwtSecurityService.java
@@ -8,21 +8,28 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JwtSecurityService {
     private static final String SECRET_KEY = "ashjkdlehfaljksdhfcxbnakljshedfnjkawlkasjhdgfasjdfgasjdgfwakvbszmjhdbvlasmehfkljahsdkljfhasdkljfhalsjkdfh";
+    SignatureAlgorithm signatureAlgorithm;
+    byte[] secretKeyBytes;
+    Key signingKey;
+    public JwtSecurityService(){
+        signatureAlgorithm = SignatureAlgorithm.HS512;
+        secretKeyBytes = Base64.getEncoder().encode(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+    }
+
 
     public String createToken(String originToken, Long expTime) {
         if(expTime <= 0) {
             throw new RuntimeException("Expired Time must exceed zero");
         }
-
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS512;
-        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
-        Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
 
         return Jwts.builder()
                 .setSubject(originToken)
@@ -33,7 +40,7 @@ public class JwtSecurityService {
 
     public String getToken(String token) {
         Claims claims = Jwts.parserBuilder()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .setSigningKey(signingKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
